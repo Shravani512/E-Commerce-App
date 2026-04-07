517086639c98e1925c5a0e091cb8d6b192cff198
Shravani512 <shravanitodkar512@gmail.com>
2026-04-07 18:51:30 +0530

    new

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -13,6 +13,15 @@ locals {
 
 }
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"
+    }
+  }
+}
+
 provider "aws" {
 
   region = local.region
